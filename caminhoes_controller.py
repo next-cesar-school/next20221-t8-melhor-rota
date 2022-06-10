eeca38bee22beea15db4855ead499c7568e9c594
Eduardo Luna <eduardodluna@hotmail.com>
2022-06-10 15:54:50 -0300

    Implementação rotina PUT Caminhoes

---
+++ caminhoes_controller.py
@@ -0,0 +1,50 @@
+import sqlite3
+
+def get_db_connection():
+    conn = sqlite3.connect('melhorrota.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+# Comandos GET, POST, PUT
+
+def insert_caminhao(descricao, localizacao, status):
+    db = get_db_connection()
+    cursor = db.cursor()
+    statement = "INSERT INTO caminhoes(descricao, localizacao, status) VALUES (?, ?, ?)"
+    cursor.execute(statement, [descricao, localizacao, status])
+    db.commit()
+    return True
+
+
+def update_caminhao(id, descricao, localizacao, status):
+    db = get_db_connection()
+    cursor = db.cursor()
+    statement = "UPDATE caminhoes SET descricao = ?, localizacao = ?, status = ? WHERE id = ?"
+    cursor.execute(statement, [descricao, localizacao, status, id])
+    db.commit()
+    return True
+
+
+def delete_caminhao(id):
+    db = get_db_connection()
+    cursor = db.cursor()
+    statement = "DELETE FROM caminhoes WHERE id = ?"
+    cursor.execute(statement, [id])
+    db.commit()
+    return True
+
+
+def get_by_id(id):
+    db = get_db_connection()
+    cursor = db.cursor()
+    statement = "SELECT id, descricao, localizacao, status FROM caminhoes WHERE id = ?"
+    cursor.execute(statement, [id])
+    return cursor.fetchone()
+
+
+def get_caminhoes():
+    db = get_db_connection()
+    cursor = db.cursor()
+    query = "SELECT * FROM caminhoes"
+    cursor.execute(query)
+    return cursor.fetchall()
--- init_db.py
+++ init_db.py
@@ -2,15 +2,19 @@ import sqlite3
 
 connection = sqlite3.connect('melhorrota.db')
 
-with open('melhorrota.sql') as f:
-    connection.executescript(f.read())
+def create_tables():
+    with open('melhorrota.sql') as f:
+        connection.executescript(f.read())
+    return True
+#cur = connection.cursor()
 
-cur = connection.cursor()
+#cur.execute("INSERT INTO caminhoes (descricao, localizacao, status) VALUES (?, ?, ?)", ('caminhao A', 'INT8', True))
 
-cur.execute("INSERT INTO caminhoes (descricao, localizacao, status) VALUES (?, ?, ?)", ('caminhao A', 'INT8', True))
+#cur.execute("INSERT INTO caminhoes (descricao, localizacao, status) VALUES (?, ?, ?)", ('caminhao B', 'INT6', False))
 
-cur.execute("INSERT INTO caminhoes (descricao, localizacao, status) VALUES (?, ?, ?)", ('caminhao B', 'INT6', False))
+#cur.execute("INSERT INTO caminhoes (descricao, localizacao, status) VALUES (?, ?, ?)", ('caminhao C', 'INT9', True))
 
 
-connection.commit()
-connection.close()
+#connection.commit()
+
+#connection.close()
